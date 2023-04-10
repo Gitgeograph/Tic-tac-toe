7c464e1fa0b14a2a271c7232e543a1ee332a5ec2
Gitgeograph <123035101+Gitgeograph@users.noreply.github.com>
2023-04-10 15:14:25 +0500

    Add files via upload

---
+++ Tic_tac_toe.py
@@ -0,0 +1,88 @@
+from random import randint
+
+
+print("*" * 10, "Крестики-нолики", "*" * 10)
+
+board = [['-', '-', '-'],
+         ['-', '-', '-'],
+         ['-', '-', '-']]
+
+
+def print_board(board):
+    print('  0 1 2')
+    for i in range(3):
+        print(str(i), *board[i])
+
+
+def player_input(cell):
+    while True:
+        place = input('Введите две координаты: ').split()
+        if len(place) != 2:
+            print('Введите две координаты')
+            continue
+        if not (place[0].isdigit() and place[1].isdigit):
+            print('Введите числа')
+            continue
+        x, y = map(int, place)
+        if not (0 <= x < 3 and 0 <= y < 3):
+            print('Вы вышли из диапозона')
+            continue
+        if cell[x][y] != '-':
+            print('Ячейка занята')
+            continue
+        else:
+            cell[x][y] = 'x'
+        break
+    return cell[x][y]
+
+
+def move_computer(cell):
+    while True:
+        x = randint(0, 2)
+        y = randint(0, 2)
+        if cell[x][y] != '-':
+            continue
+        else:
+            cell[x][y] = 'o'
+        break
+
+    return cell[x][y]
+
+
+def win(cell, val):
+    def check_win(a1, a2, a3, val):
+        if a1 == val and a2 == val and a3 == val:
+            return True
+
+    for i in range(3):
+        if check_win(cell[i][0], cell[i][1], cell[i][2], val) or \
+                check_win(cell[0][i], cell[1][i], cell[2][i], val) or \
+                check_win(cell[0][0], cell[1][1], cell[2][2], val) or \
+                check_win(cell[2][0], cell[1][1], cell[0][2], val):
+            return True
+    return False
+
+
+def start(board):
+    count = 0
+    while True:
+        print_board(board)
+        if count % 2 == 0:
+            player_input(board)
+            player = 'x'
+        else:
+            move_computer(board)
+            player = 'o'
+        count += 1
+
+        if win(board, player):
+            print_board(board)
+            print(f'Выиграл {player}')
+            break
+        if count == 9:
+            print_board(board)
+            print('Ничья')
+            break
+
+
+start(board)
